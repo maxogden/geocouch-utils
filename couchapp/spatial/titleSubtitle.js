a07a16a538213cae19f425d9cde546e948c11b16
Matt Blair <elsewisemedia@gmail.com>
2011-02-12 17:59:00 -0800

    Added a comment with database-specific example.
    This code is adapted from the design document use 
    for the iGeoCouch geo-browser.

--- couchapp/spatial/titleSubtitle.js
+++ couchapp/spatial/titleSubtitle.js
@@ -1,1 +1,21 @@
-function(doc) { emit(doc.geometry || {type: 'Point',coordinates: [0,0]}, {'title': (typeof(doc.properties.title) == 'undefined') ? 'Untitled' : doc.properties.title, 'subtitle': (typeof(doc.properties.subtitle) == 'undefined') ? 'None' : doc.properties.subtitle})}
+/*
+
+This is a generic version of a design document that returns title and 
+subtitle within the value key so they can be used to populate map 
+callouts without sending another request to Couch.
+
+A database or document-type-specific version could inject text, such as:
+
+function(doc) { 
+	emit(doc.geometry || {type: 'Point',coordinates: [0,0]}, 
+		{'title': (typeof(doc.station) == 'undefined') ? 'Unnamed' : 'Fire Station ' + doc.station, 
+		'subtitle': (typeof(doc.address) == 'undefined') ? 'Unknown' : doc.address})
+	}
+
+*/
+
+function(doc) { 
+	emit(doc.geometry || {type: 'Point',coordinates: [0,0]}, 
+		{'title': (typeof(doc.properties.title) == 'undefined') ? 'Untitled' : doc.properties.title, 
+		'subtitle': (typeof(doc.properties.subtitle) == 'undefined') ? 'None' : doc.properties.subtitle})
+		}
